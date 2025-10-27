65796abbbe448022b28718ab22c6368e468cb164
Edgarmls1 <edgar1macedosalazar@gmail.com>
2025-10-27 13:21:02 -0300

    validating

--- code-freezing.py
+++ code-freezing.py
@@ -1,9 +1,12 @@
+import datetime
 import logging
+import os
 import sys
 
 import yaml
 
 CONFIG_FILE = "config.yml"
+GITLAB_USER_LOGIN = os.getenv("GITLAB_USER_LOGIN")
 
 logging.basicConfig(
     format="[%(asctime)s] [%(levelname)s] - %(message)s",
@@ -26,13 +29,29 @@ def unpack_config(config: dict) -> tuple:
     
     return (bypass_group, freezing_dates)
 
+def is_user_in_bypass_group(username: str, bypass_group: list) -> bool:
+    return username in bypass_group
+
+def is_today_freezing_date(date_from: datetime.date, date_to: datetime.date) -> bool:
+    date_today = datetime.date.today()
+    return date_from <= date_today <= date_to
 
 def main():
     config = get_config(CONFIG_FILE)
     bypass_group, freezing_dates = unpack_config(config)
 
-    logging.info(bypass_group)
-    logging.info(freezing_dates)
+    if is_user_in_bypass_group(GITLAB_USER_LOGIN, bypass_group):
+        logging.info(f"{GITLAB_USER_LOGIN} is in bypass group, exiting")
+        sys.exit(0)
+    else:
+        for period, date in freezing_dates.items():
+            date_from = date.get("from")
+            date_to = date.get("to")
+            logging.info(f"validating {period} that goes from {date_from} to {date_to}")
+
+            if is_today_freezing_date(date_from, date_to):
+                logging.warning(f"the current date falls under '{period}', blocked due to code freezing period")
+                sys.exit(1)
 
 if __name__ == "__main__":
     main()
