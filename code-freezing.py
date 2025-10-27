1f1ab835db67ad683915394d8b85c813217bd7d5
Edgarmls1 <edgar1macedosalazar@gmail.com>
2025-10-26 21:00:48 -0300

    baby steps

--- code-freezing.py
+++ code-freezing.py
@@ -1,4 +1,38 @@
+import logging
+import sys
+
 import yaml
 
-with open("config.yml") as f:
-    print(yaml.safe_load(f))
+CONFIG_FILE = "config.yml"
+
+logging.basicConfig(
+    format="[%(asctime)s] [%(levelname)s] - %(message)s",
+    datefmt="%m-%d-%Y %I:%M:%S",
+    level=logging.DEBUG
+)
+
+def get_config(file_name: str) -> dict:
+    with open(file_name) as f:
+        return yaml.safe_load(f)
+
+
+def unpack_config(config: dict) -> tuple:
+    try:
+        bypass_group = config["bypass_group"]
+        freezing_dates = config["freezing_dates"]
+    except KeyError:
+        logging.error(f"one of the fields are not present: 'bypass_group' or 'freezing_dates' on file '{CONFIG_FILE}'")
+        sys.exit(1)
+    
+    return (bypass_group, freezing_dates)
+
+
+def main():
+    config = get_config(CONFIG_FILE)
+    bypass_group, freezing_dates = unpack_config(config)
+
+    logging.info(bypass_group)
+    logging.info(freezing_dates)
+
+if __name__ == "__main__":
+    main()
